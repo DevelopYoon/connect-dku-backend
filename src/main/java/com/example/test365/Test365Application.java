3276b1eaeace7b87d76368944c8691b798251394
DevelopYoon <outny125@naver.com>
2023-05-02 15:36:59 +0900

    프로젝트 세팅

---
+++ src/main/java/com/example/test365/Test365Application.java
@@ -0,0 +1,13 @@
+package com.example.test365;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test365Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test365Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test365/Test365ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test365;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test365ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
